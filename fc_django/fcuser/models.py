520255e03b9b0efc6813f6730b0c939d7be23cc5
Daniel Kim <berzzubunny@gmail.com>
2020-05-30 22:07:55 +0900

    feat : Django Online Advanced Class 0530 TIL -1 Making Shoppingmall Initialze

--- fc_django/fcuser/models.py
+++ fc_django/fcuser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Fcuser(models.Model):
+    email = models.EmailField(verbose_name='E-mail')
+    password = models.CharField(max_length = 64, verbose_name='password')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='register date')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'fastcampus_fcuser'
+        verbose_name = 'User'
+        verbose_name_plural = 'User'
--- fc_django/order/models.py
+++ fc_django/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    fcuser = models.ForeignKey('fcuser.Fcuser', on_delete=models.CASCADE, verbose_name='User')
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='Product')
+    quantity = models.IntegerField(verbose_name='Quantity')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='Register Date')
+
+    def __str__(self):
+        return str(self.fcuser) + ' ' + str(self.product)
+
+    class Meta:
+        db_table = 'fastcampus_order'
+        verbose_name = 'Order'
+        verbose_name_plural = 'Order'

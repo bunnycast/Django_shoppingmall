1990430da3981574645021b8121bbaf289cc6bd3
Daniel Kim <berzzubunny@gmail.com>
2020-05-31 23:21:17 +0900

    feat: Django Advanced Online Class 0531 TIL-5 Manage Stock by Using Transaction Module

--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -8,7 +8,7 @@ class OrderCreate(FormView):
     success_url = '/product/'
 
     def form_invalid(self, form):
-        return redirect('/prouct/', + str(form.product))
+        return redirect('/prouct/' + str(form.product))
 
     def get_form_kwargs(self, **kwargs): # form을 생성할 때 인자값(kwargs)을 받는 함수
         kw = super().get_form_kwargs(**kwargs)

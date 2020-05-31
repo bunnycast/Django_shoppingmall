f6c0ab3d250abd858a07685f77ab51730f54e398
Daniel Kim <berzzubunny@gmail.com>
2020-05-31 23:11:12 +0900

    feat: Django Advanced Online Class 0531 TIL-4 Making Order Create Page

--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
 # Create your views here.
+class OrderCreate(FormView):
+    form_class = RegisterForm
+    success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/prouct/', + str(form.product))
+
+    def get_form_kwargs(self, **kwargs): # form을 생성할 때 인자값(kwargs)을 받는 함수
+        kw = super().get_form_kwargs(**kwargs)
+        kw.update({
+            'request': self.request
+        })
+        return kw
